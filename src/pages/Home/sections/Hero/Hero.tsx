37565ee1d2e7bd1853370e0bcd02eac29d7d014b
Gabriel Massaia de Oliveira <gabrielmassaia50@gmail.com>
2024-08-23 21:56:34 -0300

    update

--- src/pages/Home/sections/Hero/Hero.tsx
+++ src/pages/Home/sections/Hero/Hero.tsx
@@ -1,15 +1,16 @@
-import { Container, Grid, styled, Typography } from "@mui/material"
+import { Button, Container, Grid, styled, Typography } from "@mui/material"
 import Avatar from "../../../../assets/images/avatar.png"
 
 
 const Hero = () => {
 
     const StyledHero = styled("div")(() => ({
-        backgroundColor: "black"
+        backgroundColor: "black",
+        height: "100vh",
     }))
 
     const StyledImg = styled("img")(() => ({
-        width: "30%",
+        width: "100%",
         borderRadius: "50%",
     }))
 
@@ -23,7 +24,10 @@ const Hero = () => {
                     <StyledImg src={Avatar}/>
                     </Grid>
                     <Grid item xs={8}>
-                       <Typography color={"primary"}>Gabriel M. Oliveira</Typography>
+                       <Typography color={"primary"} variant="h1">Gabriel M. Oliveira</Typography>
+                       <Typography color={"primary"} variant="h2">I'm a Software Engineer</Typography>
+                       <Button>Dowload CV</Button>
+                       <Button>Contact me!</Button>
                     </Grid>
                 </Grid>
             </Container>
