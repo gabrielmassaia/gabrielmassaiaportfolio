c6d8fad0b4464e06c73a77d4c7b725a567f3d857
Gabriel Massaia de Oliveira <gabrielmassaia50@gmail.com>
2024-08-26 20:51:06 -0300

    create background components, navbar, animatedBackground

--- src/pages/Home/sections/Hero/Hero.tsx
+++ src/pages/Home/sections/Hero/Hero.tsx
@@ -1,20 +1,21 @@
-import { Container, Grid, styled, Typography } from "@mui/material"
+import { Box, Container, Grid, styled, Typography } from "@mui/material"
 import Avatar from "../../../../assets/images/avatar.png"
 import DownloadIcon from '@mui/icons-material/Download'
 import MailOutlineIcon from '@mui/icons-material/MailOutline';
 import StyledButton from "../../../../components/StyledButton/StyledButton";
+import { AnimatedBackground } from "../../../../components/AnimatedBackground/AnimatedBackground";
 
 
 const Hero = () => {
 
-    const StyledHero = styled("div")(({theme}) => ({
+    const StyledHero = styled("div")(({ theme }) => ({
         backgroundColor: theme.palette.primary.main,
         height: "100vh",
         display: "flex",
         alignItems: "center",
     }))
 
-    const StyledImg = styled("img")(({theme}) => ({
+    const StyledImg = styled("img")(({ theme }) => ({
         width: "80%",
         borderRadius: "50%",
         border: `1px solid ${theme.palette.primary.contrastText}`
@@ -26,12 +27,19 @@ const Hero = () => {
             <Container maxWidth="lg">
                 <Grid container spacing={2}>
                     <Grid item xs={12} md={5}>
-                    <StyledImg src={Avatar}/>
+                        <Box position="relative">
+                            <Box position="absolute" width="150%" top="-100" right="0">
+                                <AnimatedBackground/>
+                            </Box>
+                            <Box position="relative" textAlign="center">
+                                <StyledImg src={Avatar}/>
+                            </Box>
+                        </Box>
                     </Grid>
                     <Grid item xs={12} md={7}>
-                       <Typography color="primary.contrastText" variant="h1" textAlign="center">Gabriel M. Oliveira</Typography>
+                       <Typography color="primary.contrastText" variant="h1" textAlign="center" pb={2}>Gabriel M. Oliveira</Typography>
                        <Typography color="primary.contrastText" variant="h2" textAlign="center">I'm a Software Engineer</Typography>
-                       <Grid container display="flex" justifyContent="center" spacing={3}>
+                       <Grid container display="flex" justifyContent="center" spacing={3} pt={3}>
                             <Grid item xs={12} md={4} display="flex" justifyContent="center">
                                 <StyledButton>
                                     <DownloadIcon/>
