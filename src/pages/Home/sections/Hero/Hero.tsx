f07694daba6a140fce16e7e16d0d4ce15cc78b7a
Gabriel Massaia de Oliveira <gabrielmassaia50@gmail.com>
2024-08-26 20:16:55 -0300

    alinhei os componentes

--- src/pages/Home/sections/Hero/Hero.tsx
+++ src/pages/Home/sections/Hero/Hero.tsx
@@ -1,37 +1,57 @@
-import { Button, Container, Grid, styled, Typography } from "@mui/material"
+import { Container, Grid, styled, Typography } from "@mui/material"
 import Avatar from "../../../../assets/images/avatar.png"
+import DownloadIcon from '@mui/icons-material/Download'
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
+import StyledButton from "../../../../components/StyledButton/StyledButton";
 
 
 const Hero = () => {
 
-    const StyledHero = styled("div")(() => ({
-        backgroundColor: "black",
+    const StyledHero = styled("div")(({theme}) => ({
+        backgroundColor: theme.palette.primary.main,
         height: "100vh",
+        display: "flex",
+        alignItems: "center",
     }))
 
-    const StyledImg = styled("img")(() => ({
-        width: "100%",
+    const StyledImg = styled("img")(({theme}) => ({
+        width: "80%",
         borderRadius: "50%",
+        border: `1px solid ${theme.palette.primary.contrastText}`
     }))
 
     return (
       <>
         <StyledHero>
-
-            <Container>
+            <Container maxWidth="lg">
                 <Grid container spacing={2}>
-                    <Grid item xs={4}>
+                    <Grid item xs={12} md={5}>
                     <StyledImg src={Avatar}/>
                     </Grid>
-                    <Grid item xs={8}>
-                       <Typography color={"primary"} variant="h1">Gabriel M. Oliveira</Typography>
-                       <Typography color={"primary"} variant="h2">I'm a Software Engineer</Typography>
-                       <Button>Dowload CV</Button>
-                       <Button>Contact me!</Button>
+                    <Grid item xs={12} md={7}>
+                       <Typography color="primary.contrastText" variant="h1" textAlign="center">Gabriel M. Oliveira</Typography>
+                       <Typography color="primary.contrastText" variant="h2" textAlign="center">I'm a Software Engineer</Typography>
+                       <Grid container display="flex" justifyContent="center" spacing={3}>
+                            <Grid item xs={12} md={4} display="flex" justifyContent="center">
+                                <StyledButton>
+                                    <DownloadIcon/>
+                                    <Typography>
+                                    Dowload CV
+                                    </Typography>
+                                </StyledButton>
+                            </Grid>
+                            <Grid item xs={12} md={4} display="flex" justifyContent="center">
+                                <StyledButton>
+                                <MailOutlineIcon/>
+                                <Typography>
+                                Contact me!
+                                </Typography>
+                                </StyledButton>
+                            </Grid>
+                       </Grid>
                     </Grid>
                 </Grid>
             </Container>
-
         </StyledHero>
       </>
     )
